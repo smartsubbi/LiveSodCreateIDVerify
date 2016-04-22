d73c9523872bf4b65a5618b1b09dd396a096f99e
Subbu <subbu.baliga@live.in>
2016-04-22 11:44:18 +0530

    Updated sendmail with email id

--- LiveLoginTestCases/src/test/java/ReUse/SendMail.java
+++ LiveLoginTestCases/src/test/java/ReUse/SendMail.java
@@ -22,7 +22,7 @@ public class SendMail
 		Thread.sleep(5000);		
 		driver.findElement(By.xpath("//div[.='COMPOSE']")).click();		
 		Thread.sleep(5000);		
-		driver.findElement(By.id(":na")).sendKeys("smartsubbi@gmail.com");		
+		driver.findElement(By.id(":na")).sendKeys(emailIdToSend);		
 		Thread.sleep(5000);		
 		driver.findElement(By.id(":kc")).click();		
 		Thread.sleep(5000);	
